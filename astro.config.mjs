0eaefa507031cbff0f045488651c71c0fdc4b08c
incconu_two <89911479+incconutwo@users.noreply.github.com>
2026-07-14 00:49:50 +0100

    Fix Vercel settings and Vite build warnings, configure canonical URL, sitemap, and Ko-fi links

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   vite: {
+    build: {
+      chunkSizeWarningLimit: 1500,
+    },
     server: {
       proxy: {
         '/api': {
